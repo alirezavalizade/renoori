a49bd83df47eac10102cf8a7278eb7f3ab5cdde3
alireza valizade <alirezavalizade0@gmail.com>
2018-03-13 00:46:51 +0330

    add src/utils/generateRandomString.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import domElements from './utils/domElements';
+import randomString from './utils/generateRandomString';
 
 const stylesKeyManager = {};
 
 function domElemntsGenerator(){
     const components = {};
     domElements.forEach((el) => {
-        components[el] = (styles) => {
-
-            console.log(styles);
-
+        components[el] = (styles = {}) => {
+            Object.keys(styles).forEach((style) => {
+                if (!stylesKeyManager[style]) {
+                    
+                }
+            });
+            
             return ({ ...props, children }) => React.createElement(el, props, children);
         };
     });
